410cd505d56c27d8d03683df0826e9bfaa62e3e6
Luke Hur <lukehur@Lukes-MBP.lan>
2017-03-14 11:41:01 +0900

    Add files

---
+++ ex22.py
@@ -0,0 +1,40 @@
+print = it is a statement or function
++ = plus
+- = minus
+/ = slash
+* = asterisk
+% = percent
+< = less-than
+> = greater-than
+<= less-than-equal
+>= greater-than-equal
+_ = underscore character
+%r = saying "print this no matter what"
+() = parenthesis
+, = commas
+\\	Backslash (\)
+\'	Single-quote (')
+\"	Double-quote (")
+\a	ASCII bell (BEL)
+\b	ASCII backspace (BS)
+\f	ASCII formfeed (FF)
+\n	ASCII linefeed (LF)
+\N{name}	Character named name in the Unicode database (Unicode only)
+\r	Carriage Return (CR)
+\t	Horizontal Tab (TAB)
+\uxxxx	Character with 16-bit hex value xxxx (u'' string only)
+\Uxxxxxxxx	Character with 32-bit hex value xxxxxxxx (u'' string only)
+\v	ASCII vertical tab (VT)
+\ooo	Character with octal value ooo
+\xhh	Character with hex value hh
+raw_input = wait until the user type and press Return
+q = quit python
+pydoc = name of a function, module, or package, or a dotted reference to a class, method, or function within a module or module in a package.
+import = import a stuff from a file
+argv = argument
+close -- Closes the file. Like File->Save.. in your editor.
+read -- Reads the contents of the file. You can assign the result to a variable.
+readline -- Reads just one line of a text file.
+truncate -- Empties the file. Watch out if you care about the file.
+write('stuff') -- Writes "stuff" to the file.
+: = colon
